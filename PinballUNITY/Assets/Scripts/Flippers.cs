41bbbde65bfed7ae27c365feb37288da76746a1d
FodaGamez <alberto.alves@escolar.ifrn.edu.br>
2025-06-10 17:54:00 -0300

    FINALMENTE

--- PinballUNITY/Assets/Scripts/Flippers.cs
+++ PinballUNITY/Assets/Scripts/Flippers.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
 
-public class Flippers : MonoBehaviour
+public class FlipperController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public HingeJoint2D leftFlipper;
+public HingeJoint2D rightFlipper;
+public float motorSpeed = 1000f;
+public float motorMaxTorque = 10000f;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+void Update()
+{
+// Controla o flipper esquerdo (A ou seta esquerda)
+if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+{
+SetMotor(leftFlipper, -motorSpeed);
+}
+else
+{
+SetMotor(leftFlipper, motorSpeed);
+}
+
+// Controla o flipper direito (D ou seta direita)
+if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+{
+SetMotor(rightFlipper, motorSpeed);
+}
+else
+{
+SetMotor(rightFlipper, -motorSpeed);
+}
+}
+
+void SetMotor(HingeJoint2D hinge, float speed)
+{
+JointMotor2D motor = hinge.motor;
+motor.motorSpeed = speed;
+motor.maxMotorTorque = motorMaxTorque;
+hinge.motor = motor;
+}
 }
